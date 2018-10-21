fc4a52b8d093ad447986d17e97cea6e683697e98
CesarValladares <a01023506@itesm.mx>
2018-10-21 01:52:44 -0500

    Tarea 4

--- Ecualization.cpp
+++ Ecualization.cpp
@@ -5,10 +5,70 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <math.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
 #include "omp.h"
+
+using namespace std;
+using namespace cv;
+
+void histog(Mat &image, Mat &output){
+
+    int x = image.cols;
+    int y = image.rows;
+
+    long totalSize = x*y;
+
+    cout << totalSize << endl;
+
+    int hist[256] ={};
+
+    cout << "Calculando histograma" << endl;
+    // Calculando histograma
+    for (int i = 0; i < y; i++){
+
+        for (int j = 0; j < x; j++){
+
+            unsigned int index = (int)image.at<uchar>(i,j);
+
+            hist[index]++;
+        }
+    }
+
+    cout << "Normalizando" << endl;
+    // Normalizando 
+    int hist_s[256]= {};
+
+    for (int i = 0; i < 256; i++){
+        
+        for(int j = 0; j <= i; j++){
+            
+            hist_s[i] += hist[j];
+        }
+
+        unsigned int aux  = (hist_s[i]*255) /totalSize;
+
+        hist_s[i] = aux;
+    }
+
+    cout << "Imagen final" << endl;
+    // Rellenando la imagen final 
+
+    for (int i = 0; i < y; i++){
+        
+        for(int j = 0; j < x; j++){
+
+            unsigned int index = (int)image.at<uchar>(i,j);
+
+            output.at<uchar>(i,j) = hist_s[index];
+        }
+    }
+
+}
 
 
 int main (int argc, char** argv){
+
 
     if (argc < 2){
          
@@ -17,18 +77,27 @@ int main (int argc, char** argv){
     else {
         
         Mat image;
-        Mat grayImage;
+        Mat grayImage(image.rows,image.cols,CV_8UC1);
+        Mat output(image.rows,image.cols,CV_8UC1);
 
         image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
+        cout << "Image size Step: "<< image.step << " Rows: " << image.rows << " Cols: " << image.cols << endl;
+
         cvtColor(image, grayImage, CV_BGR2GRAY);
 
-        namedWindow("Original", cv::WINDOW_NORMAL);
+        output = grayImage.clone();
+
+        histog(grayImage, output);
+        
+        namedWindow("Input", cv::WINDOW_NORMAL);
         namedWindow("Output", cv::WINDOW_NORMAL);
 
-        imshow("Original", image);
-        imshow("Output", grayImage);
+        imshow("Input", grayImage);
+        imshow("Output", output);
+    }   
 
-    }
+    waitKey(0);
+    return 0;
 
 }
